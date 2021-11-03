831bb8f2589f18ed21ac7aa962c967c85bc1fe38
Philip Justin Cochangco <88139560+pcochangco@users.noreply.github.com>
2021-11-03 19:43:01 +0800

    Update EC_Calibration.py

--- GreenPonik_EC_Python/example/EC_Calibration.py
+++ GreenPonik_EC_Python/example/EC_Calibration.py
@@ -1,7 +1,7 @@
 import time
 import sys
-sys.path.insert(0,'DFRobot_ADS1115/RaspberryPi/Python/')
-sys.path.insert(0,'GreenPonik_EC_Python/src/')
+sys.path.insert(0,'Aquaponics/DFRobot_ADS1115/RaspberryPi/Python/')
+sys.path.insert(0,'Aquaponics/GreenPonik_EC_Python/src/')
 
 from DFRobot_ADS1115 import ADS1115
 from GreenPonik_EC import GreenPonik_EC
