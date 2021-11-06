ee428abb60950a5cf89b5afc8466d3a1c8725209
Philip Justin Cochangco <88139560+pcochangco@users.noreply.github.com>
2021-11-06 19:02:22 +0800

    Update EC_PH.py

--- EC_PH.py
+++ EC_PH.py
@@ -73,7 +73,7 @@ def read_ph_ec():
 
 
 ads1115 = ADS1115()
-GPIO_init():
+GPIO_init()
 input("Connect EC sensor Vdd pin to pin 16 of raspberry pi. GND pin to any ground of raspi. Press Enter to continue")
 print("")
 input("Connect PH sensor Vdd pin to pin 18 of raspberry pi. GND pin to any ground of raspi. Press Enter to continue")
